772ad825bd88a66f8f620da4d515339daf2e1c0d
parkcool <yickle1345@naver.com>
2024-08-04 06:09:10 +0000

    Feat: Add `checkNameIsUsing` model

--- src/model/user/checkNameIsUsing.ts
+++ src/model/user/checkNameIsUsing.ts
@@ -5,15 +5,16 @@ import type { RowDataPacket } from "mysql2";
 interface ResultRow {
   result: 1;
 }
-
-export interface CheckIdIsUsingProps {
-  id: string;
+export interface CheckNameIsUsingProps {
+  name: string;
 }
 
-export default async function checkIdIsUsing({ id }: CheckIdIsUsingProps) {
+export default async function checkNameIsUsing({
+  name,
+}: CheckNameIsUsingProps) {
   const queryResult = await db.query<(ResultRow & RowDataPacket)[]>(
     "SELECT 1 AS result FROM user WHERE ? LIMIT 1",
-    { id }
+    { name }
   );
 
   return queryResult;
