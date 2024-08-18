cc1978e05aa872701c60f310ebdad30d4fa0e6d7
parkcool <yickle1345@naver.com>
2024-08-18 08:58:41 +0000

    Feat: Change `error` to `code` in `userErrorHandler`

--- src/middleware/error/userErrorHandler.ts
+++ src/middleware/error/userErrorHandler.ts
@@ -1,10 +1,18 @@
 import { Request, Response, NextFunction } from "express";
 
 import UserError from "error/UserError";
+import ResponseCode from "constant/responseCode";
 
-export default function userErrorHandler(error: UserError, req: Request, res: Response, next: NextFunction) {
+export default function userErrorHandler(
+  error: UserError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
   if (error instanceof UserError) {
-    return res.status(error.statusCode).json({ message: error.message, error: error.name });
+    return res
+      .status(error.statusCode)
+      .json({ message: error.message, code: ResponseCode.FAILURE });
   } else {
     return next(error);
   }
