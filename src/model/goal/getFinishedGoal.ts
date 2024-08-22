0c3b25839d1d64347cf48af2c76d314e6f6001cb
parkcool <yickle1345@naver.com>
2024-08-22 10:43:16 +0000

    Feat: Add `getFinishedGoal` model

---
+++ src/model/goal/getFinishedGoal.ts
@@ -0,0 +1,44 @@
+import db from "model";
+
+import type { GoalChecklistRow } from "db";
+import type { RowDataPacket } from "mysql2";
+
+export interface FinishedGoalRow extends GoalChecklistRow {
+  goalName: string;
+  isMain: boolean;
+}
+
+export interface GetFinishedGoalProps {
+  userId: string;
+  date: string;
+}
+
+export default async function getFinishedGoal({
+  userId,
+  date,
+}: GetFinishedGoalProps) {
+  const queryResult = await db.query<(FinishedGoalRow & RowDataPacket)[]>(
+    `
+      SELECT
+          gc.id,
+          gc.goalId,
+          gc.content,
+          gc.isDone,
+          gc.createdAt,
+          g.name AS goalName
+          g.isMain AS isMain
+      FROM 
+          goal_checklist gc
+      JOIN 
+          goal g
+      ON 
+          gc.goalID = g.id
+      WHERE
+          gc.userId = ? AND
+          gc.date < ?;
+    `,
+    [userId, date]
+  );
+
+  return queryResult;
+}
