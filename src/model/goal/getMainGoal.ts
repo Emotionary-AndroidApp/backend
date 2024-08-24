8ff7028682be73fc869ea7fa7a5403796db71692
parkcool <yickle1345@naver.com>
2024-08-24 04:54:36 +0000

    feat: Improve `getMainGoal` query to calculate progress correctly

--- src/model/goal/getMainGoal.ts
+++ src/model/goal/getMainGoal.ts
@@ -15,22 +15,24 @@ export default async function getMainGoal({ userId }: GetMainGoalProps) {
   const queryResult = await db.query<(MainGoalRow & RowDataPacket)[]>(
     `
       SELECT
-        goal.id,
-        goal.userId,
-        goal.name,
-        goal.isMain,
-        goal.start,
-        goal.end,
-        goal.createdAt,
-        SUM(checklist.isDone) / COUNT(checklist.id) AS progress
+          goal.id,
+          goal.userId,
+          goal.name,
+          goal.isMain,
+          goal.start,
+          goal.end,
+          goal.createdAt,
+          COALESCE(SUM(checklist.isDone), 0) / NULLIF(COUNT(checklist.id), 0) AS progress
       FROM
-        goal
+          goal
       LEFT JOIN
-        goal_checklist AS checklist
+          goal_checklist AS checklist
       ON
-        goal.id = checklist.goalId
+          goal.id = checklist.goalId
       WHERE
-        goal.userId = ? AND goal.isMain = true
+          goal.userId = ? AND goal.isMain = true
+      GROUP BY
+          goal.id, goal.userId, goal.name, goal.isMain, goal.start, goal.end, goal.createdAt
     `,
     [userId]
   );
