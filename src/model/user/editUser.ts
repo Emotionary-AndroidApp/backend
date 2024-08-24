869ca4af1904afa2a0b20d3010f999cd341ef0b9
parkcool <yickle1345@naver.com>
2024-08-24 03:37:42 +0000

    Feat: Add `editUser` model

---
+++ src/model/user/editUser.ts
@@ -0,0 +1,23 @@
+import db from "model";
+
+import type { ResultSetHeader } from "mysql2";
+import type { UserRow } from "db";
+
+type UserPatch = Pick<UserRow, "password" | "name" | "picture">;
+
+interface EditUserProps {
+  userId: number;
+  userPatch: Partial<UserPatch>;
+}
+
+export default async function editUser({ userId, userPatch }: EditUserProps) {
+  if (Object.keys(userPatch).length === 0)
+    throw new Error("userPatch is empty");
+
+  const queryResult = await db.query<ResultSetHeader>(
+    "UPDATE user SET ? WHERE id = ? LIMIT 1",
+    [userPatch, userId]
+  );
+
+  return queryResult;
+}
